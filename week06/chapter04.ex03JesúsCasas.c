2d9684fe4b0e59ae5e13c8605478a6d541f7a615
JesusCasas024 <84023296+JesusCasas024@users.noreply.github.com>
2021-06-15 17:38:25 -0500

    Create chapter04.ex03JesúsCasas.c

---
+++ week06/chapter04.ex03JesúsCasas.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+int main()
+  
+{
+  
+  int w,h,a,p; /* w=widht,h=height,a=area, p=perimeter*/
+
+  printf("w=3);
+  scanf("%d",&w);
+
+  printf("h=5");
+  scanf("%d",&h);
+  
+  p=h*2 + w*2;
+         
+  printf("the result of perimeter is= %d", p); /* the result is in inches*/
+         
+  a=w*h;
+  
+   printf("the result of area is= %d", a); /*the result is in inches*/
+   
+  printf("if we want to change the values to numbers with decimal we need to change float in place of int \n");
+         
+  printf(" Then we change the values of widht and height to make the program works with the new values");
+         
+  return 0;
+         
+  }
+
+  
+  
