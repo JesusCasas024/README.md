9be501227a78696a7bc8f7f13f04175ea7b6da9d
JesusCasas024 <84023296+JesusCasas024@users.noreply.github.com>
2021-06-15 18:10:40 -0500

    Update chapter04.ex05JesúsCasas.c

--- week06/chapter04.ex05JesúsCasas.c
+++ week06/chapter04.ex05JesúsCasas.c
@@ -4,4 +4,20 @@ int main()
   
 {
   
+  float w;
+  char name[10];
+  int a;
   
+  printf("w=5.7");
+  scanf("%d",w);
+  
+  printf("name=jesús");
+    scanf("%d",name);
+  
+  printf("a=8");
+  scanf("%f",a);
+  
+  
+return 0;
+  
+}
