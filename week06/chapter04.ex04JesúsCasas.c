e3777136773def434f4d8c051bcfa2d7917fff40
JesusCasas024 <84023296+JesusCasas024@users.noreply.github.com>
2021-06-15 17:57:19 -0500

    Update chapter04.ex04JesúsCasas.c

--- week06/chapter04.ex04JesúsCasas.c
+++ week06/chapter04.ex04JesúsCasas.c
@@ -4,17 +4,17 @@ int main()
   
 {
   
-  printf("H   H EEEEE L     L      OOO ");
+  printf("H   H EEEEE L     L      OOO \n ");
 
-  printf("H   H E     L     L     O   O");
+  printf("H   H E     L     L     O   O\n");
 
-  printf("H   H E     L     L     O   O");
+  printf("H   H E     L     L     O   O\n");
 
-  printf("HHHHH EEEEE L     L     O   O");
+  printf("HHHHH EEEEE L     L     O   O\n");
 
-  printf("H   H E     L     L     O   O");
+  printf("H   H E     L     L     O   O\n");
 
-  printf("H   H E     L     L     O   O");
+  printf("H   H E     L     L     O   O\n");
 
   printf("H   H EEEEE LLLLL LLLLL  OOO ");
 
