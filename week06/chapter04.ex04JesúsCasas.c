b6b66c62eff091549005e331b06d7f966f553ebc
JesusCasas024 <84023296+JesusCasas024@users.noreply.github.com>
2021-06-15 17:52:27 -0500

    Create chapter04.ex04JesúsCasas.c

---
+++ week06/chapter04.ex04JesúsCasas.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main()
+  
+{
+  
+  printf("H   H EEEEE L     L      OOO ");
+
+  printf("H   H E     L     L     O   O");
+
+  printf("H   H E     L     L     O   O");
+
+  printf("HHHHH EEEEE L     L     O   O");
+
+  printf("H   H E     L     L     O   O");
+
+  printf("H   H E     L     L     O   O");
+
+  printf("H   H EEEEE LLLLL LLLLL  OOO ");
+
+return 0;
+
+}
