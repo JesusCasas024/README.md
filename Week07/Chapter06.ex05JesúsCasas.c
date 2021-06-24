e962aa1f5334354cf78f2a4225ab8f93a6fcf2ba
JesusCasas024 <84023296+JesusCasas024@users.noreply.github.com>
2021-06-23 19:42:14 -0500

    Update Chapter06.ex05JesúsCasas.c

--- Week07/Chapter06.ex05JesúsCasas.c
+++ Week07/Chapter06.ex05JesúsCasas.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 
 int main(){
+    char line_text;
     int year;
 
     printf("put a year: ");
-    scanf("%d", &year);
+    gets(line_text, sizeof(line_text), stdin);       
+    sscanf(line_text, "%d", &year, line_text);
+    
 
     if(year % 4 != 0 || (year % 100==0 && year % 400!=0)){
-        printf("its not leap");
+        printf("The year: %d", year "is not a lap year");
     }
     else {
-        printf("is a leap year");
+        printf("The year: %d", year, "is a leap year");
     }
     return 0;
 }
